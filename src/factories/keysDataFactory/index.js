81a414f9f73b796ddf64ce3d2813a3febd3a5b3b
Stef <3736753+SFERRER-DEV@users.noreply.github.com>
2023-01-10 16:42:04 +0100

    Ajouter l'usine  pour fabriquer des utilisateurs et celle pour fabriquer les données clés des utilisateurs

---
+++ src/factories/keysDataFactory/index.js
@@ -0,0 +1,28 @@
+import KeyData, { Keys } from '../../models/KeyData';
+/** @typedef {import('../../utils/context/typedef').KeyDataJSON} KeyDataJSON Raccourci pour importer des types des propriétés JSON */
+
+/**
+ * Usine pour fabriquer un utilisateur avec ses données clefs à partir des données JSON
+ * @param {KeyDataJSON} json Les données JSON d'un utilisateur
+ * @returns {Array.<KeyData>} Un tableau des données clés typées KeyData
+ */
+function keysDataFactory(json) {
+  const { calorieCount, proteinCount, carbohydrateCount, lipidCount } = json;
+
+  /**
+   *
+   * @returns {Array.<KeyData>}
+   */
+  function getKeysData() {
+    const keysData = [];
+    keysData.push(new KeyData('calorieCount', calorieCount));
+    keysData.push(new KeyData('proteinCount', proteinCount));
+    keysData.push(new KeyData('carbohydrateCount', carbohydrateCount));
+    keysData.push(new KeyData('lipidCount', lipidCount));
+    return keysData;
+  }
+
+  return { getKeysData };
+}
+
+export default keysDataFactory;
